f03d8619f4263f6e46d2b19cfe7290d8739028cc
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-06-06 13:59:44 -0700

    leave todos

---
+++ internal/vfs/match.go
@@ -0,0 +1,5 @@
+package vfs
+
+func matchFilesNew(path string, extensions []string, excludes []string, includes []string, useCaseSensitiveFileNames bool, currentDirectory string, depth *int, host FS) []string {
+	panic("unimplemented")
+}
