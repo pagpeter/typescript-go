43607f2d2b40506bfa32166c77424208c618e251
Peet <gh@peet.ws>
2025-07-01 21:03:12 +0200

    chore: fix script

--- externalize.sh
+++ externalize.sh
@@ -11,3 +11,27 @@ find . -name "*.go" -type f -exec sed -i '' 's|github\.com/microsoft/typescript-
 echo "Replacement complete!"
 echo "Files modified:"
 find . -name "*.go" -type f -exec grep -l "github.com/pagpeter/typescript-go" {} \;
+
+# Rename the internal directory to external
+if [ -d "./internal" ]; then
+    mv ./internal ./external
+    echo "Renamed ./internal to ./external"
+else
+    echo "Directory ./internal not found"
+fi
+
+# Delete the cmd directory
+if [ -d "./cmd" ]; then
+    rm -rf ./cmd
+    echo "Deleted ./cmd directory"
+else
+    echo "Directory ./cmd not found"
+fi
+
+# Update go.mod file
+if [ -f "./go.mod" ]; then
+    sed -i '' 's|github\.com/microsoft/typescript-go|github.com/pagpeter/typescript-go|g' ./go.mod
+    echo "Updated ./go.mod"
+else
+    echo "File ./go.mod not found"
+fi
