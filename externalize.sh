9452b0183287c0d987ed751a4bc09f8c62e205d1
Peet <gh@peet.ws>
2025-07-01 21:10:06 +0200

    chore: fix sed command

--- externalize.sh
+++ externalize.sh
@@ -3,10 +3,10 @@
 # Script to recursively replace import paths in Go files
 
 # Replace the more specific path first to avoid partial replacements
-find . -name "*.go" -type f -exec sed -i '' 's|github\.com/microsoft/typescript-go/internal|github.com/pagpeter/typescript-go/external|g' {} \;
+find . -name "*.go" -type f -exec sed -i 's|github\.com/microsoft/typescript-go/internal|github.com/pagpeter/typescript-go/external|g' {} \;
 
 # Replace the general path
-find . -name "*.go" -type f -exec sed -i '' 's|github\.com/microsoft/typescript-go|github.com/pagpeter/typescript-go|g' {} \;
+find . -name "*.go" -type f -exec sed -i 's|github\.com/microsoft/typescript-go|github.com/pagpeter/typescript-go|g' {} \;
 
 echo "Replacement complete!"
 echo "Files modified:"
@@ -30,7 +30,7 @@ fi
 
 # Update go.mod file
 if [ -f "./go.mod" ]; then
-    sed -i '' 's|github\.com/microsoft/typescript-go|github.com/pagpeter/typescript-go|g' ./go.mod
+    sed -i 's|github\.com/microsoft/typescript-go|github.com/pagpeter/typescript-go|g' ./go.mod
     echo "Updated ./go.mod"
 else
     echo "File ./go.mod not found"
